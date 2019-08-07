a5ede56f72c2365c0f368415a641ff9979565cd9
David Morgan-Brown <david@morgan-brown.com>
2019-08-07 18:39:12 +0100

    DMB, knocking out of the park since 1972™️

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,8 +1,60 @@
-import {format} from '../src/dateFormatter';
+import {format, formatHomeScreen} from '../src/dateFormatter';
 
-it('when the system date and the date to format are the same day formats as "TODAY"', () => {
-  const December = 11; //js Date object month is indexed from 0
-  const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
-  const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime(); //Same day different time
-  expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+
+it('Past dates are formatted as dd/MM/yyyy strings', () => {
+  expect(
+    format(Date.parse('2018-10-01T00:00:00.000Z'), Date.parse('2019-01-01T12:00:00.000Z'))
+  ).toBe('01/10/2018');
+});
+
+it('Future dates are formatted as dd/MM/yyyy strings', () => {
+  expect(
+    format(Date.parse('2020-10-01T00:00:00.000Z'), Date.parse('2019-01-01T12:00:00.000Z'))
+  ).toBe('01/10/2020');
+});
+
+it('Today is formatted as "TODAY"', () => {
+  expect(
+    format(Date.parse('2019-01-01T00:00:00.000Z'), Date.parse('2019-01-01T12:00:00.000Z'))
+  ).toBe('TODAY');
+});
+
+it('Today is formatted as time on the home screen', () => {
+  expect(
+    formatHomeScreen(Date.parse('2019-01-01T11:32:15.000Z'), Date.parse('2019-01-01T12:00:00.000Z'))
+  ).toBe('11:32');
+});
+
+it('Yesterday is formatted as "YESTERDAY"', () => {
+  expect(
+    format(Date.parse('2018-12-31T12:00:00.000Z'), Date.parse('2019-01-01T12:00:00.000Z'))
+  ).toBe('YESTERDAY');
+  expect(
+    format(Date.parse('2018-07-10T12:00:00.000Z'), Date.parse('2018-07-11T12:00:00.000Z'))
+  ).toBe('YESTERDAY');
+  expect(
+    format(Date.parse('2018-07-31T12:00:00.000Z'), Date.parse('2018-08-01T12:00:00.000Z'))
+  ).toBe('YESTERDAY');
+});
+
+
+it('3-6 days behind to be formatted as d/o/w', () => {
+  expect(
+    format(Date.parse('2019-01-11T12:00:00.000Z'), Date.parse('2019-01-12T12:00:00.000Z'))
+  ).toBe('YESTERDAY');
+  expect(
+    format(Date.parse('2019-01-10T12:00:00.000Z'), Date.parse('2019-01-12T12:00:00.000Z'))
+  ).toBe('THU');
+  expect(
+    format(Date.parse('2019-01-09T12:00:00.000Z'), Date.parse('2019-01-12T12:00:00.000Z'))
+  ).toBe('WED');
+  expect(
+    format(Date.parse('2019-01-08T12:00:00.000Z'), Date.parse('2019-01-12T12:00:00.000Z'))
+  ).toBe('TUE');
+  expect(
+    format(Date.parse('2019-01-07T12:00:00.000Z'), Date.parse('2019-01-12T12:00:00.000Z'))
+  ).toBe('MON');
+  expect(
+    format(Date.parse('2019-01-06T12:00:00.000Z'), Date.parse('2019-01-12T12:00:00.000Z'))
+  ).toBe('06/01/2019');
 });
--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,40 @@
-export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+
+const formatDMY = (date) => {
+  let year = date.getFullYear();
+  let month = (1 + date.getMonth()).toString().padStart(2, '0');
+  let day = date.getDate().toString().padStart(2, '0');
+
+  return `${day}/${month}/${year}`;
 };
+
+const formatHM = (date) => {
+  let hours = date.getHours().toString().padStart(2, '0');
+  let mins  = date.getMinutes().toString().padStart(2, '0');
+
+  return `${hours}:${mins}`;
+};
+
+const formatRule = (offset, dateFormat) => (dateToFormatTimeMillis, systemDateTimeMillis) => {
+  let dateToFormat = new Date(dateToFormatTimeMillis);
+  let offsetDate   = new Date(systemDateTimeMillis);
+  offsetDate.setDate(offsetDate.getDate() + offset);
+  return formatDMY(offsetDate) === formatDMY(dateToFormat) ? [dateFormat(dateToFormat)] : [];
+};
+
+const textFormatRule = (offset, text) => formatRule(offset, () => text);
+const dow = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const dowFormatRule = (offset) => formatRule(offset, date => dow[date.getDay()]);
+
+const todayRule     = textFormatRule(0, 'TODAY');
+const todayTimeRule = formatRule(0, formatHM);
+const yesterdayRule = textFormatRule(-1, 'YESTERDAY');
+const dayOfWeekRule = [-5, -4, -3, -2].map(dowFormatRule);
+const defaultRule   = dateTimeMillis => formatDMY(new Date(dateTimeMillis));
+
+const commonRules = [yesterdayRule, ...dayOfWeekRule, defaultRule];
+
+const formatUsingRules = (rules) => (dateToFormatTimeMillis, systemDateTimeMillis) => rules.flatMap(rule => rule(dateToFormatTimeMillis, systemDateTimeMillis))[0];
+
+export const format           = formatUsingRules([todayRule, ...commonRules]);
+export const formatHomeScreen = formatUsingRules([todayTimeRule, ...commonRules]);
+
