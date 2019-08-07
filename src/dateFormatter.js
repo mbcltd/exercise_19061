eb5156ef6ebce1f3c9ed7c4229ed1a40ff2f9cf1
David Morgan-Brown <david@morgan-brown.com>
2019-08-07 18:45:58 +0100

    More descriptive function names, fixed tab spacing.

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -8,35 +8,37 @@ const formatDayMonthYear = (date) => {
 
 const formatHourMinutes = (date) => {
   let hours = date.getHours().toString().padStart(2, '0');
-  let mins  = date.getMinutes().toString().padStart(2, '0');
+  let mins = date.getMinutes().toString().padStart(2, '0');
 
   return `${hours}:${mins}`;
 };
 
-const formatRule = (dayOffset, dateFormat) => (dateToFormatTimeMillis, systemDateTimeMillis) => {
+const formatWhenDateMatchesOffset = (dayOffset, dateFormat) => (dateToFormatTimeMillis, systemDateTimeMillis) => {
   let dateToFormat = new Date(dateToFormatTimeMillis);
-  let offsetDate   = new Date(systemDateTimeMillis);
+  let offsetDate = new Date(systemDateTimeMillis);
   offsetDate.setDate(offsetDate.getDate() + dayOffset);
   return formatDayMonthYear(offsetDate) === formatDayMonthYear(dateToFormat)
-      ? [dateFormat(dateToFormat)]
-      : [];
+    ? [dateFormat(dateToFormat)]
+    : [];
 };
 
-const dow = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-const dowFormatRule = (offset) => formatRule(offset, date => dow[date.getDay()]);
+const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const dayOfWeekFormatWhenDateMatchesOffset =
+  (offset) =>
+    formatWhenDateMatchesOffset(offset, date => daysOfWeek[date.getDay()]);
 
-const todayRule     = formatRule(0, () => 'TODAY');
-const todayTimeRule = formatRule(0, formatHourMinutes);
-const yesterdayRule = formatRule(-1, () => 'YESTERDAY');
-const dayOfWeekRule = [-5, -4, -3, -2].map(dowFormatRule);
+const todayRule     = formatWhenDateMatchesOffset(0, () => 'TODAY');
+const todayTimeRule = formatWhenDateMatchesOffset(0, formatHourMinutes);
+const yesterdayRule = formatWhenDateMatchesOffset(-1, () => 'YESTERDAY');
+const dayOfWeekRule = [-5, -4, -3, -2].map(dayOfWeekFormatWhenDateMatchesOffset);
 const defaultRule   = dateTimeMillis => formatDayMonthYear(new Date(dateTimeMillis));
 
 const commonRules = [yesterdayRule, ...dayOfWeekRule, defaultRule];
 
 const formatUsingRules = (rules) =>
-    (dateToFormatTimeMillis, systemDateTimeMillis) =>
-        rules.flatMap(rule => rule(dateToFormatTimeMillis, systemDateTimeMillis))[0];
+  (dateToFormatTimeMillis, systemDateTimeMillis) =>
+    rules.flatMap(rule => rule(dateToFormatTimeMillis, systemDateTimeMillis))[0];
 
-export const format           = formatUsingRules([todayRule, ...commonRules]);
+export const format = formatUsingRules([todayRule, ...commonRules]);
 export const formatHomeScreen = formatUsingRules([todayTimeRule, ...commonRules]);
 
